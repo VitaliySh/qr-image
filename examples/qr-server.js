9d1a1eb1bcad75a179e3f20d79910e230e0990bc
VitaliySh <vitaliy.sh@gmail.com>
2017-03-27 09:41:48 +0500

    qr-server.js modified

--- examples/qr-server.js
+++ examples/qr-server.js
@@ -2,14 +2,20 @@ var http = require('http');
 var url = require('url');
 var qr = require('../');
 
-http.createServer(function (req, res) {
-    var text = url.parse(req.url, true).query.text;
-    try {
-        var img = qr.image(text);
-        res.writeHead(200, {'Content-Type': 'image/png'});
-        img.pipe(res);
-    } catch (e) {
-        res.writeHead(414, {'Content-Type': 'text/html'});
-        res.end('<h1>414 Request-URI Too Large</h1>');
+http.createServer(function (request, response) {
+    if(request.method === 'GET' && request.url.substring(0, 9) == '/qr?text='){
+        var text = decodeURI(url.parse(request.url, true).query.text);
+        try {
+            console.log(text);
+            var img = qr.image(text);
+            response.writeHead(200, {'Content-Type': 'image/png'});
+            img.pipe(response);
+        } catch (e) {
+            response.writeHead(414, {'Content-Type': 'text/html'});
+            response.end('<h1>414 Request-URI Too Large</h1>');
+        }
+    } else {
+        response.statusCode = 404;
+        response.end();
     }
 }).listen(5152);
